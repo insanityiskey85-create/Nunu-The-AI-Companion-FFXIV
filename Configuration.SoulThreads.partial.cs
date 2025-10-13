a1be5d3fc6773a93a05ed4f463b567986a7a89c9
insanityiskey85-create <insanityiskey85@gmail.com>
2025-10-14 00:28:07 +0100

    fix another one for building the plugin

--- Configuration.SoulThreads.partial.cs
+++ Configuration.SoulThreads.partial.cs
@@ -7,9 +7,7 @@ namespace NunuTheAICompanion
         public float ThreadSimilarityThreshold { get; set; } = 0.78f;
         public int ThreadContextMaxFromThread { get; set; } = 6;
         public int ThreadContextMaxRecent { get; set; } = 8;
-    }
+
         public string? ChatEndpointUrl { get; set; } = "http://localhost:11434";
-    }
-        public int ContextTurns { get; set; } = 12;
     }
 }
